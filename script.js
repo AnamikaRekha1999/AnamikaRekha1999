2edf7561ed17c2a2d217acd7d12cc6b286a41313
Anamika Rekha <ar3876@drexel.edu>
2024-09-17 16:52:35 -0400

    Updating Work Experience Script

--- script.js
+++ script.js
@@ -138,9 +138,10 @@ const data = fetch(resume)
       worksHtml.innerHTML += `<hr>`;
       worksHtml.innerHTML += `<h4>${list.position} - ${list.name}</h4>`;
 
-      worksHtml.innerHTML += `<span class="worksSpan">${list.startDate} - ${list.location}</span>`;
+      worksHtml.innerHTML += `<span class="worksSpan">${list.startDate} - ${list.endDate}</span> - ${list.location}</span>`;
 
       worksHtml.innerHTML += `<p>${list.summary}</p>`;
+      worksHtml.innerHTML += `<p>${list.}</p>`;
     }
 
     //educationHtml studyType
